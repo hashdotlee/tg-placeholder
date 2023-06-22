7b08d818a9315a3437d7c154fea4363faa41fe1e
Lê Viết Hoàng <31377641+hashdotlee@users.noreply.github.com>
2023-06-22 17:23:42 +0000

    Update

--- src/steps/StrategySession.jsx
+++ src/steps/StrategySession.jsx
@@ -1,9 +1,11 @@
+import { InlineWidget } from 'react-calendly'
 import VideoComponent from './VideoComponent'
 
 export default function StrategySession() {
     return <>
         <VideoComponent url="https://youtu.be/wufTNcID8vc" />
-        <div style={{marginTop: "16px", width: "100%"}}>
-<iframe src="https://app.hellobonsai.com/f/2d767823a1d1421?embed=true" frameborder="0" width="100%" height="100%" style={{border: "none", minHeight: "600px"}}></iframe>        </div>
+        <div style={{ marginTop: "16px", width: "100%" }}>
+            <InlineWidget url="https://calendly.com/tigen-agency/strategy-session" />
+        </div>
     </>
 }
